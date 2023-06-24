6b3928bfda594f9b6223ca1122318c625ad61cc7
Zach Snyder <sadin@fedoraproject.org>
2023-06-24 18:16:01 -0400

    implemented readChar() in the lexer

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -10,5 +10,16 @@ type Lexer struct {
 
 func New(input string) *Lexer {
 	l := &Lexer{input: input}
+	l.readChar()
 	return l
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosi:tion
+	l.readPosition += 1
 }
